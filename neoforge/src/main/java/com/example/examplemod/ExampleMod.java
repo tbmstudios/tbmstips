7f96ec9fc7b70fd6624911b1995ea5cf38855591
TheButterbrotMan <thebutterbrotman@gmail.com>
2024-12-16 11:32:20 +0100

    Initial commit

---
+++ neoforge/src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,14 @@
+package com.example.examplemod;
+
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ExampleMod {
+
+    public ExampleMod(IEventBus eventBus) {
+        CommonClass.init();
+
+    }
+}
